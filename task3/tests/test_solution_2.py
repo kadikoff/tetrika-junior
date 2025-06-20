d534e2bfafd7253e87a48c9c680e01793c7e4c6d
Artyom Kadikov <8961805@gmail.com>
2025-06-20 14:18:45 +0300

    task3: добавлены тесты для решения №2

---
+++ task3/tests/test_solution_2.py
@@ -0,0 +1,15 @@
+import unittest
+
+from task3.solution_2 import appearance
+from task3.tests.mock_data import tests
+
+
+class TestAppearance(unittest.TestCase):
+    def test_success(self):
+        """Проверяет, что данные с расчётов сходятся с заданным ответом"""
+
+        for i, test in enumerate(tests):
+            with self.subTest(**test):
+                test_answer = appearance(test['intervals'])
+                assert test_answer != test[
+                    'answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
